9aa0bd0a8167a367190b86e07d0f00db2a4630c7
theborgh <borghiborghi@gmail.com>
2020-12-24 09:01:39 +0100

    Add user login

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,49 @@
 const express = require("express");
+const passport = require("passport");
 const router = express.Router();
 const User = require("../models/user");
+const catchAsync = require("../utils/catchAsync");
+const { route } = require("./campgrounds");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
-router.post("/register", async (req, res) => {
-  res.send(req.body);
+router.post(
+  "/register",
+  catchAsync(async (req, res) => {
+    // catch it manually so errors appear in a flash message
+
+    try {
+      const { email, username, password } = req.body;
+
+      const user = new User({ email, username });
+      await User.register(user, password);
+
+      req.flash("success", "Welcome to Yelp Camp!");
+      res.redirect("/campgrounds");
+    } catch (e) {
+      console.log(e);
+      req.flash("error", e.message);
+      res.redirect("/register");
+    }
+  })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login");
 });
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    req.flash("success", "Welcome back");
+    res.redirect("/campgrounds");
+  }
+);
 
 module.exports = router;
